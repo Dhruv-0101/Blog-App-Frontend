20f6b65a3bfe724c3036bc96dd42086e0be1b140
Dhruv patel <d7511162@gmail.com>
2024-05-26 01:45:38 +0530

    admin-related

--- src/components/User/UserDashboard.jsx
+++ src/components/User/UserDashboard.jsx
@@ -1,3 +1,258 @@
+// import { Fragment, useState } from "react";
+// import { Dialog, Transition } from "@headlessui/react";
+// import {
+//   Cog6ToothIcon,
+//   HomeIcon,
+//   XMarkIcon,
+// } from "@heroicons/react/24/outline";
+// import { FaBlog } from "react-icons/fa6";
+// import { Link, Outlet } from "react-router-dom";
+// import { useQuery } from "@tanstack/react-query";
+
+// import {
+//   FaUserEdit,
+//   FaFileAlt,
+//   FaUsers,
+//   FaCalendarPlus,
+//   FaTags,
+//   FaWallet,
+// } from "react-icons/fa";
+// import { userProfileAPI } from "../../APIServices/users/usersAPI";
+
+// const navigation = [
+//   { name: "Dashboard", href: "/dashboard", icon: HomeIcon, current: true },
+//   {
+//     name: "Create New Post",
+//     href: "/dashboard/create-post",
+//     icon: FaUserEdit,
+//     current: false,
+//   },
+//   {
+//     name: "My Posts",
+//     href: "/dashboard/posts",
+//     icon: FaFileAlt,
+//     current: false,
+//   },
+//   {
+//     name: "My Followers",
+//     href: "/dashboard/my-followers",
+//     icon: FaUsers,
+//     current: false,
+//   },
+//   {
+//     name: "My Followings",
+//     href: "/dashboard/my-followings",
+//     icon: FaUsers,
+//     current: false,
+//   },
+//   {
+//     name: "Create Plan",
+//     href: "/dashboard/create-plan",
+//     icon: FaCalendarPlus,
+//     current: false,
+//   },
+//   {
+//     name: "Add Category",
+//     href: "/dashboard/add-category",
+//     icon: FaTags,
+//     current: false,
+//   },
+//   {
+//     name: "My Earnings",
+//     href: "/dashboard/my-earnings",
+//     icon: FaWallet,
+//     current: false,
+//   },
+// ];
+
+// function classNames(...classes) {
+//   return classes.filter(Boolean).join(" ");
+// }
+
+// export default function UserDashbaord() {
+//   const [sidebarOpen, setSidebarOpen] = useState(false);
+//   //Get the auth user from redux store
+
+//   const { data: profileData, refetch: refetchProfile } = useQuery({
+//     queryKey: ["profile-photo"],
+//     queryFn: () => userProfileAPI(),
+//   });
+//   console.log(profileData?.user?.isAdmin);
+
+//   return (
+//     <>
+//       {/* Account summary component */}
+
+//       <div>
+//         <Transition.Root show={sidebarOpen} as={Fragment}>
+//           <Dialog
+//             as="div"
+//             className="relative z-50 lg:hidden"
+//             onClose={setSidebarOpen}
+//           >
+//             <Transition.Child
+//               as={Fragment}
+//               enter="transition-opacity ease-linear duration-300"
+//               enterFrom="opacity-0"
+//               enterTo="opacity-100"
+//               leave="transition-opacity ease-linear duration-300"
+//               leaveFrom="opacity-100"
+//               leaveTo="opacity-0"
+//             >
+//               <div className="fixed inset-0 bg-gray-900/80" />
+//             </Transition.Child>
+
+//             <div className="fixed inset-0 flex">
+//               <Transition.Child
+//                 as={Fragment}
+//                 enter="transition ease-in-out duration-300 transform"
+//                 enterFrom="-translate-x-full"
+//                 enterTo="translate-x-0"
+//                 leave="transition ease-in-out duration-300 transform"
+//                 leaveFrom="translate-x-0"
+//                 leaveTo="-translate-x-full"
+//               >
+//                 <Dialog.Panel className="relative mr-16 flex w-full max-w-xs flex-1">
+//                   <Transition.Child
+//                     as={Fragment}
+//                     enter="ease-in-out duration-300"
+//                     enterFrom="opacity-0"
+//                     enterTo="opacity-100"
+//                     leave="ease-in-out duration-300"
+//                     leaveFrom="opacity-100"
+//                     leaveTo="opacity-0"
+//                   >
+//                     <div className="absolute left-full top-0 flex w-16 justify-center pt-5">
+//                       <button
+//                         type="button"
+//                         className="-m-2.5 p-2.5"
+//                         onClick={() => setSidebarOpen(false)}
+//                       >
+//                         <span className="sr-only">Close sidebar</span>
+//                         <XMarkIcon
+//                           className="h-6 w-6 text-white"
+//                           aria-hidden="true"
+//                         />
+//                       </button>
+//                     </div>
+//                   </Transition.Child>
+
+//                   <div className="flex grow flex-col gap-y-5 overflow-y-auto bg-white px-6 pb-4">
+//                     <div className="flex h-16 shrink-0 items-center">
+//                       {/* Logo */}
+//                       <Link to="/">
+//                         <FaBlog className="h-8 w-auto text-orange-500" />
+//                       </Link>
+//                     </div>
+//                     <nav className="flex flex-1 flex-col">
+//                       <ul role="list" className="flex flex-1 flex-col gap-y-7">
+//                         <li>
+//                           <ul role="list" className="-mx-2 space-y-1">
+//                             {navigation.map((item) => (
+//                               <li key={item.name}>
+//                                 <Link
+//                                   to={item.href}
+//                                   className={classNames(
+//                                     item.current
+//                                       ? "bg-gray-50 text-orange-600"
+//                                       : "text-orange-700 hover:text-indigo-600 hover:bg-gray-50",
+//                                     "group flex gap-x-3 rounded-md p-2 text-sm leading-6 font-semibold"
+//                                   )}
+//                                 >
+//                                   <item.icon
+//                                     className={classNames(
+//                                       item.current
+//                                         ? "text-indigo-600"
+//                                         : "text-gray-400 group-hover:text-indigo-600",
+//                                       "h-6 w-6 shrink-0"
+//                                     )}
+//                                     aria-hidden="true"
+//                                   />
+//                                   {item.name}
+//                                 </Link>
+//                               </li>
+//                             ))}
+//                           </ul>
+//                         </li>
+//                       </ul>
+//                     </nav>
+//                   </div>
+//                 </Dialog.Panel>
+//               </Transition.Child>
+//             </div>
+//           </Dialog>
+//         </Transition.Root>
+
+//         {/* Static sidebar for desktop */}
+//         <div className="hidden lg:fixed lg:inset-y-0 lg:z-50 lg:flex lg:w-72 lg:flex-col">
+//           <div className="flex grow flex-col gap-y-5 overflow-y-auto border-r border-gray-200 bg-white px-6 pb-4">
+//             <div className="flex h-16 shrink-0 items-center">
+//               {/* Logo */}
+//               <Link to="/">
+//                 <FaBlog className="h-8 w-auto text-orange-500" />
+//               </Link>
+//             </div>
+//             <nav className="flex flex-1 flex-col">
+//               <ul role="list" className="flex flex-1 flex-col gap-y-7">
+//                 <li>
+//                   <ul role="list" className="-mx-2 space-y-1">
+//                     {navigation.map((item) => (
+//                       <li key={item.name}>
+//                         <Link
+//                           to={item.href}
+//                           className={classNames(
+//                             item.current
+//                               ? "bg-gray-50 text-orange-600"
+//                               : "text-gray-700 hover:text-indigo-600 hover:bg-gray-50",
+//                             "group flex gap-x-3 rounded-md p-2 text-sm leading-6 font-semibold"
+//                           )}
+//                         >
+//                           <item.icon
+//                             className={classNames(
+//                               item.current
+//                                 ? "text-orange-600"
+//                                 : "text-gray-400 group-hover:text-orange-600",
+//                               "h-6 w-6 shrink-0"
+//                             )}
+//                             aria-hidden="true"
+//                           />
+//                           {item.name}
+//                         </Link>
+//                       </li>
+//                     ))}
+//                   </ul>
+//                 </li>
+
+//                 <li className="mt-auto">
+//                   <Link
+//                     to="/dashboard/settings"
+//                     className="group -mx-2 flex gap-x-3 rounded-md p-2 text-sm font-semibold leading-6 text-gray-700 hover:bg-gray-50 hover:text-indigo-600"
+//                   >
+//                     <Cog6ToothIcon
+//                       className="h-6 w-6 shrink-0 text-gray-400 group-hover:text-indigo-600"
+//                       aria-hidden="true"
+//                     />
+//                     Settings
+//                   </Link>
+//                 </li>
+//               </ul>
+//             </nav>
+//           </div>
+//         </div>
+
+//         <div className="lg:pl-72">
+//           <main className="py-10">
+//             <div className="px-4 sm:px-6 lg:px-8">
+//               {/* Your content */}
+
+//               <Outlet />
+//             </div>
+//           </main>
+//         </div>
+//       </div>
+//     </>
+//   );
+// }
 import { Fragment, useState } from "react";
 import { Dialog, Transition } from "@headlessui/react";
 import {
@@ -46,12 +301,6 @@ const navigation = [
     current: false,
   },
   {
-    name: "Create Plan",
-    href: "/dashboard/create-plan",
-    icon: FaCalendarPlus,
-    current: false,
-  },
-  {
     name: "Add Category",
     href: "/dashboard/add-category",
     icon: FaTags,
@@ -63,21 +312,34 @@ const navigation = [
     icon: FaWallet,
     current: false,
   },
+  // This item will be conditionally rendered based on the isAdmin flag
+  {
+    name: "Create Plan",
+    href: "/dashboard/create-plan",
+    icon: FaCalendarPlus,
+    current: false,
+    adminOnly: true, // Custom property to indicate admin-only item
+  },
 ];
 
 function classNames(...classes) {
   return classes.filter(Boolean).join(" ");
 }
 
-export default function UserDashbaord() {
+export default function UserDashboard() {
   const [sidebarOpen, setSidebarOpen] = useState(false);
-  //Get the auth user from redux store
 
+  // Fetch user profile data
   const { data: profileData, refetch: refetchProfile } = useQuery({
     queryKey: ["profile-photo"],
     queryFn: () => userProfileAPI(),
   });
-  console.log(profileData);
+  const isAdmin = profileData?.user?.isAdmin;
+
+  // Filter navigation items based on user role
+  const filteredNavigation = navigation.filter(
+    (item) => !item.adminOnly || isAdmin
+  );
 
   return (
     <>
@@ -148,7 +410,7 @@ export default function UserDashbaord() {
                       <ul role="list" className="flex flex-1 flex-col gap-y-7">
                         <li>
                           <ul role="list" className="-mx-2 space-y-1">
-                            {navigation.map((item) => (
+                            {filteredNavigation.map((item) => (
                               <li key={item.name}>
                                 <Link
                                   to={item.href}
@@ -196,7 +458,7 @@ export default function UserDashbaord() {
               <ul role="list" className="flex flex-1 flex-col gap-y-7">
                 <li>
                   <ul role="list" className="-mx-2 space-y-1">
-                    {navigation.map((item) => (
+                    {filteredNavigation.map((item) => (
                       <li key={item.name}>
                         <Link
                           to={item.href}
